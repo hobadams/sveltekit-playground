14b0c13cde2e7fda525031fbcdb7b666834c3fcc
Hob Adams <hob.adams@motivepartners.com>
2023-12-07 09:56:25 +0000

    add tan stack package + add posts component

---
+++ src/components/Post.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { Post } from '../types/post';
+
+	export let post: Post;
+</script>
+
+<article class="rounded bg-slate-200 p-4">
+	<h3 class="font-semibold mb-4">{post.title}</h3>
+	<p>{post.body}</p>
+</article>
---
+++ src/routes/data/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import type { Post as PostType } from '../../types/post';
+	import Post from '../../components/Post.svelte';
+
+	let posts: PostType[] = [];
+	onMount(async () => {
+		const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+		posts = await res.json();
+	});
+</script>
+
+<h2 class="font-bold text-lg">Posts</h2>
+<ul class="max-w-[500px]">
+	{#each posts as post}
+		<li class="mb-4">
+			<Post {post} />
+		</li>
+	{/each}
+</ul>
