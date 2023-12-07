c2cf38742cf0aa5681ce6774083a051b297ca078
ademola.aboluwarin <ademola.aboluwarin@motivepartners.com>
2023-12-07 14:18:24 +0000

    adds skeleton ui

--- src/components/CreatePostForm.svelte
+++ src/components/CreatePostForm.svelte
@@ -47,7 +47,7 @@
     <div class="md:w-1/3"></div>
     <div class="md:w-2/3">
       <button
-        class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+        class="btn variant-filled"
         type="submit"
       >
         Submit
