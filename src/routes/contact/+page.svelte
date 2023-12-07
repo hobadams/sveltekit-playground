4ecaacad67244cbb63ba9c827ee7c89b15d05e90
Elliot Wood <43952853+ElliotWood13@users.noreply.github.com>
2023-12-07 14:23:09 +0000

    bit of cleanup

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,22 +1,40 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
 	import { superForm, superValidateSync } from 'sveltekit-superforms/client';
 	import SuperDebug from 'sveltekit-superforms/client/SuperDebug.svelte';
 	import { z } from 'zod';
 
 	const formSchema = z.object({
-		firstName: z.string().min(1),
-		lastName: z.string().min(1),
-		email: z.string().email().min(1)
+		firstName: z
+			.string()
+			.min(1)
+			.refine((firstName) => firstName.length < 1, {
+				message: 'First name is required',
+				path: ['firstName']
+			}),
+		lastName: z
+			.string()
+			.min(1)
+			.refine((lastName) => lastName.length < 1, {
+				message: 'Last name is required',
+				path: ['lastName']
+			}),
+		email: z
+			.string()
+			.email()
+			.min(1)
+			.refine((email) => email.length < 1, {
+				message: 'Email is required',
+				path: ['email']
+			})
 	});
 
 	const data = superValidateSync(formSchema);
-	const { form, errors, validate } = superForm(data);
+	const { form, errors, enhance } = superForm(data, {
+		validators: formSchema
+	});
 
 	const handleSubmit = async () => {
-		const test = await validate('firstName');
 		console.log($form);
-		console.log({ test });
 	};
 </script>
 
@@ -25,6 +43,8 @@
 </div>
 <div class="max-w-md mx-auto">
 	<form
+		method="POST"
+		use:enhance
 		on:submit|preventDefault={handleSubmit}
 		class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
 	>
@@ -37,7 +57,7 @@
 				aria-invalid={$errors.firstName ? 'true' : undefined}
 				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
 			/>
-			{#if $errors.firstName}<span class="text-red">First name required</span>{/if}
+			{#if $errors.firstName}<span class="text-red">{$errors.firstName}</span>{/if}
 		</div>
 
 		<div class="mb-4">
@@ -53,7 +73,7 @@
 		</div>
 
 		<div class="mb-4">
-			<label for="email" class="block text-gray-700 text-sm font-bold mb-2">E-mail</label>
+			<label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email:</label>
 			<input
 				type="email"
 				name="email"
