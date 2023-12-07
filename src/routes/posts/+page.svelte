1aeb3703b91df3286ef69cc6ae8c2e92b27ef155
ademola.aboluwarin <ademola.aboluwarin@motivepartners.com>
2023-12-07 11:34:15 +0000

    extract create post form out

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,82 +1,32 @@
 <script lang="ts">
-	import PostsList from "../../components/PostsList.svelte";
-  import { postsStore } from "$lib/stores/post-store";
+	import PostsList from '../../components/PostsList.svelte';
+	import { postsStore } from '$lib/stores/post-store';
+	import CreatePostForm from '../../components/CreatePostForm.svelte';
 
 	// your script goes here
 	let newTitle = '';
 	let newDetails = '';
-  let postList: { title: string, details: string }[] = []
+	let postList: { title: string; details: string }[] = [];
 
-  function addToList() {
-    console.log("=====", { newTitle, newDetails })
-    postList = [...postList, { title: newTitle, details: newDetails }]
-    postsStore.addNewPost(newTitle, newDetails);
-    newTitle = ""
-    newDetails = ""
-  }
+	function addToList() {
+		postList = [...postList, { title: newTitle, details: newDetails }];
+		postsStore.addNewPost(newTitle, newDetails);
+		newTitle = '';
+		newDetails = '';
+	}
 </script>
 
 <div class="flex w-full gap-8">
-  <a href="/" class="text-sky-400">Home</a>
-  <form class="w-1/2" on:submit|preventDefault={addToList}>
-    <div class="md:flex md:items-center mb-6">
-      <div class="md:w-1/3">
-        <label
-          class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-          for="inline-post-title"
-        >
-          Post Title
-        </label>
-      </div>
-      <div class="md:w-2/3">
-        <input
-          class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-          id="inline-post-title"
-          type="text"
-          placeholder="Enter your title..."
-          bind:value={newTitle}
-        />
-      </div>
-    </div>
-    <div class="md:flex md:items-center mb-6">
-      <div class="md:w-1/3">
-        <label
-          class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-          for="inline-details"
-        >
-          Details
-        </label>
-      </div>
-      <div class="md:w-2/3">
-        <input
-          class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-          id="inline-details"
-          type="text"
-          placeholder="Enter details"
-          bind:value={newDetails}
-        />
-      </div>
-    </div>
-    <div class="md:flex md:items-center">
-      <div class="md:w-1/3"></div>
-      <div class="md:w-2/3">
-        <button
-          class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-          type="submit"
-        >
-          Submit
-        </button>
-      </div>
-    </div>
-  </form>
+	<a href="/" class="text-sky-400">Home</a>
+	<CreatePostForm bind:title={newTitle} bind:details={newDetails} handleSubmit={addToList} />
 
-  <aside class="w-1/2 flex flex-col">
-    <!-- rendering from app state -->
-    <PostsList posts={postList} heading="Posts from component state"/>
-    <div class="border-solid border-4 border-gray-600 mt-2 mb-3" />
-    <!-- rendering from store -->
-    <PostsList posts={$postsStore} heading="Posts from Svelte store"/>
-  </aside>
+	<aside class="w-1/2 flex flex-col">
+		<!-- rendering from app state -->
+		<PostsList posts={postList} heading="Posts from component state" />
+		<div class="border-solid border-4 border-gray-600 mt-2 mb-3" />
+		<!-- rendering from store -->
+		<PostsList posts={$postsStore} heading="Posts from Svelte store" />
+	</aside>
 </div>
 
 <style>
