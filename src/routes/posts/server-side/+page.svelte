646bf817ce067a1d52d7f1eda9a0ff57da4324a9
Hob Adams <hob.adams@motivepartners.com>
2023-12-07 10:06:59 +0000

    adding ssr routes

---
+++ src/routes/posts/server-side/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { PageLoad } from './$types';
+
+	export const load: PageLoad = async () => {
+		const posts = await getPosts();
+		return { posts };
+	};
+</script>
